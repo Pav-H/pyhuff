3a4519d1ae02ba8fb721de49a28e9b4e423dd409
pkhomutov <p.khomutov@gmail.com>
2020-12-01 22:20:15 +0300

    refactoring using the Node class so far it's the same for the simple case with one line

--- main.py
+++ main.py
@@ -1,92 +1,61 @@
 # -*- coding: utf8 -*-
 
-# print("Enter a string")
-# str_in = input()
-str_in = "aaabbccd aabbcd"
 
-letters_and_freq = []  # символы и их частота [a, 1, b, 2,...]
-letters_used = []  # используемые символы
-for symbol in str_in:
-    if symbol not in letters_and_freq:
-        frequency = str_in.count(symbol)
-        letters_and_freq.append(frequency)
-        letters_and_freq.append(symbol)
-        letters_used.append(symbol)
-print("letters_and_freq = ", letters_and_freq)
-print("letters_arr = ", letters_used)
+class Node(object):
+    left = None
+    right = None
 
-nodes = []  # список узлов [ [a,1], [b, 2],... ]
-while len(letters_and_freq) > 0:
-    nodes.append(letters_and_freq[0:2])
-    letters_and_freq = letters_and_freq[2:]
-nodes.sort()
-print("nodes after sort", nodes)
-tree = [nodes]
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
+    def get_leaves(self):
+        return self.left, self.right
 
-# обьединение узлов в дерево и установка пометок 0 и 1 каждой паре узлов
-def combine_nodes(nodes):
-    position = 0
-    new_node = []
-    if len(nodes) > 1:
-        nodes.sort()
-        nodes[position].append("0")
-        nodes[position + 1].append("1")
-        combined_node1 = (nodes[position][0] + nodes[position + 1][0])
-        combined_node2 = (nodes[position][1] + nodes[position + 1][1])
-        # print("combined_node1 = ", combined_node1)
-        # print("combined_node2 = ", combined_node2)
-        new_node.append(combined_node1)
-        new_node.append(combined_node2)
-        # print("new_node.append = ", new_node)
-        new_node_arr = [new_node]
-        new_node_arr = new_node_arr + nodes[2:]
-        # print("new_node_arr = ", new_node_arr)
-        nodes = new_node_arr
-        tree.append(nodes)
-        # print("tree.append = ", tree)
-        # print()
-        combine_nodes(nodes)
-    return tree
-
-
-new_nodes = combine_nodes(nodes)
-tree.sort(reverse=True)
-print("tree.sort = ", tree)
-
-# удаление дубликатов
-count_arr = []
-for tier in tree:  # ярус
-    for node in tier:  # узел
-        if node not in count_arr:
-            count_arr.append(node)
-        else:
-            tier.remove(node)
-print("tree after del = ", tree)
-
-# вывод дерева
-print()
-count = 0
-for tier in tree:
-    print("tier", count, " = ", tier)
-    count += 1
 
 # кодирование
-binary_code = []
-if len(letters_used) == 1:
-    code_letter = [letters_used[0], "0"]
-    binary_code.append(code_letter * len(str_in))
-else:
-    for symbol in letters_used:
-        code_symbol = ""
-        for node in count_arr:
-            if len(node) > 2 and symbol in node[1]:
-                code_symbol = code_symbol + node[2]
-        code_letter = [symbol, code_symbol]
-        binary_code.append(code_letter)
+def make_huffman_code(node: Node, binary_code=""):
+    if type(node) is str:
+        return {node: binary_code}
+    (left, right) = node.get_leaves()
+    d = {}
+    if left is not None:
+        d.update(make_huffman_code(left, binary_code + "0"))
+    if right is not None:
+        d.update(make_huffman_code(right, binary_code + "1"))
+    return d
 
-# вывод кодов для символов
-print()
-print("symbol   code")
-for symbol_code in binary_code:
-    print(symbol_code[0], "      ", symbol_code[1])
+
+if __name__ == "__main__":
+    str_in = "aaabbccd aabbcd"
+    symbols_and_freq = {}  # символы и их частота {'a': 1, 'b': 2,...}
+    for symbol in str_in:
+        if symbol in symbols_and_freq:
+            symbols_and_freq[symbol] += 1
+        else:
+            symbols_and_freq[symbol] = 1
+    print("symbols_and_freq = ", symbols_and_freq)
+
+    symbols_and_freq = sorted(symbols_and_freq.items(), key=lambda f: f[1], reverse=True)
+    nodes_arr = symbols_and_freq
+    print("nodes_arr = ", nodes_arr)
+    while len(nodes_arr) > 1:
+        (key1, c1) = nodes_arr[-1]
+        (key2, c2) = nodes_arr[-2]
+        nodes_arr = nodes_arr[:-2]
+        node = Node(key1, key2)
+        nodes_arr.append((node, c1 + c2))
+        nodes_arr = sorted(nodes_arr, key=lambda x: x[1], reverse=True)
+
+    if nodes_arr[0][0].get_leaves is None:
+        code = {nodes_arr[0][0]: "0"}  # если строка из одного символа
+    else:
+        code = make_huffman_code(nodes_arr[0][0])
+    print("code = ", code)
+    print("Char     Code")
+    for (symbol, frequency) in symbols_and_freq:
+        first, second = (symbol, code[symbol])
+        print(first, " =    ", second)
+
+# TODO: чтение из файлов
+# TODO: использовать multiprocessing для параллельной обработки нескольких файлов
